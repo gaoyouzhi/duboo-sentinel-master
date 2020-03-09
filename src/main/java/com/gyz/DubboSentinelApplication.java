7a7869908beab6379efb83efa019bf2f33b8d7be
youzhi.gao <youzhi.gao@ikang.com>
2020-03-09 10:59:51 +0800

    整合dubbo-sentinell

---
+++ src/main/java/com/gyz/DubboSentinelApplication.java
@@ -0,0 +1,17 @@
+package com.gyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class DubboSentinelApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(DubboSentinelApplication.class);
+    }
+}
